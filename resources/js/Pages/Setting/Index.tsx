379737202277caf235db78ffce1f2cbc3dc0cd01
yurisi <Dev.yurisi@gmail.com>
2023-11-25 01:50:51 +0900

    fix design

--- resources/js/Pages/Setting/Index.tsx
+++ resources/js/Pages/Setting/Index.tsx
@@ -42,34 +42,37 @@ export default function Index(props: any) {
         >
             <Head title="設定 - UniverseWeb"/>
 
-            <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 pb-5">
-                <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg py-5">
-                    <h2 className="text-3xl text-center mb-5">設定</h2>
-                    <h3 className="text-center mb-3">プロフィール編集</h3>
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 pb-5">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg py-5">
+                        <h2 className="text-3xl text-center mb-5">設定</h2>
+                        <h3 className="text-center mb-3">プロフィール編集</h3>
 
-                    <div className="sm:px-0 lg:px-8 w-full text-center">
-                        <form onSubmit={submit}>
-                            <div className="text-sm text-red-500">
-                                {error}
-                            </div>
-                            <FormControl sx={{width: "100%"}}>
-                                <FormLabel sx={{textAlign: "left"}}>自己紹介</FormLabel>
-                                <Textarea placeholder="迷ったら自分の好きなことを伝えてみよう！" minRows={4}
-                                          required={true}
-                                          slotProps={{textarea: {maxLength: 400}}}
-                                          onChange={e => setData('introduction', e.target.value)}
-                                          defaultValue={props.profile.introduction}
-                                />
-                                <FormHelperText sx={{textAlign: "right"}}>400文字以内</FormHelperText>
-                            </FormControl>
-                            <Button type="submit" variant="contained">保存</Button>
-                        </form>
-                    </div>
+                        <div className="sm:px-0 lg:px-8 w-full text-center">
+                            <form onSubmit={submit}>
+                                <div className="text-sm text-red-500">
+                                    {error}
+                                </div>
+                                <FormControl sx={{width: "100%"}}>
+                                    <FormLabel sx={{textAlign: "left"}}>自己紹介</FormLabel>
+                                    <Textarea placeholder="迷ったら自分の好きなことを伝えてみよう！" minRows={4}
+                                              required={true}
+                                              slotProps={{textarea: {maxLength: 400}}}
+                                              onChange={e => setData('introduction', e.target.value)}
+                                              defaultValue={props.profile.introduction}
+                                    />
+                                    <FormHelperText sx={{textAlign: "right"}}>400文字以内</FormHelperText>
+                                </FormControl>
+                                <Button type="submit" variant="contained">保存</Button>
+                            </form>
+                        </div>
 
-                    <div className="sm:px-0 lg:px-8 w-full">
-                        <p className="text-center mt-10">その他設定</p>
-                        <FormControlLabel
-                            control={<Switch checked={lastLoginOption} onChange={handleLastLoginOptionChange}/>} label="最終ログイン日時表示設定"/>
+                        <div className="sm:px-0 lg:px-8 w-full">
+                            <p className="text-center mt-10">その他設定</p>
+                            <FormControlLabel
+                                control={<Switch checked={lastLoginOption} onChange={handleLastLoginOptionChange}/>}
+                                label="最終ログイン日時表示設定"/>
+                        </div>
                     </div>
                 </div>
             </div>
